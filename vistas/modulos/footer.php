27040c9f0d97f6581380f4430314415fb77a6793
neitan-02 <147124117+neitan-02@users.noreply.github.com>
2025-05-03 16:17:52 -0600

    actualizacion de apartados

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2025 <a href="https://www.tutorialesatualcance.com" target="_blank">Practicando proyectos</a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
---
+++ vistas/modulos/inicio.php
@@ -0,0 +1,39 @@
+<div class="content-wrapper">
+    <section class="content-header">
+        <h1>Tablero
+            <small>Panel de Control</small>
+
+        </h1>
+
+        <o1 class="breadcrumb">
+
+            <li><a href="#"><i class="fa fa-dashboard"></i>Inicio</a></li>
+            
+            <li class="active">Tablero</li>
+        </o1>
+
+    </section>
+
+    <section class="content">
+        <div class="box">
+            <div class="box-header with-border">
+                <h3 class="box.title">Titulo</h3>
+
+                <div class="box-tools pull-right">
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"
+                        data-toggle="tooltip" title="Collapse"> <i class="fa fa-minus"></i></button>
+
+                    <button type="button" class="btn btn btn-box-tool" data-widget="remove"
+                        data-toggle="tooltip" title="Remove">
+                        <i class="fa fa-times"></i></button>
+                </div>
+            </div>
+            <div class="box-body">
+                start creating
+            </div>
+            <div class="box-footer">
+                Footer
+            </div>
+        </div>
+    </section>
+</div>
